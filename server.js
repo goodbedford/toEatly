e9d27c13306044cf49493e9660f032ae6ea2e0ea
goodbedford <bedford@goodbedford.com>
2015-10-14 12:22:50 -0700

    added post for adding food

--- server.js
+++ server.js
@@ -23,11 +23,32 @@ var foods =[
   {id: 4, name: "Kale", yumminess: "meh"}
 ];
 
+function maxId(arr) {
+  var max = arr[0].id;
+  arr.forEach(function(food){
+    if( food.id > max){
+      max = food.id;
+    }
+  });
+  return max;
+}
 
 //GET root
 app.get('/', function(req, res){
 
   res.render('index',{foods:foods});
+});
+
+//POST food
+app.post('/foods', function(req, res){
+  var newFood = {};
+  newFood.id = maxId(foods);
+  newFood.name = req.body.foodName;
+  newFood.yumminess = req.body.foodYumminess;
+
+  console.log(req.body)
+  foods.push( newFood);
+  res.render('index', {foods:foods});
 });
 app.listen(3000, function (){
   console.log("listening on port 3000");
