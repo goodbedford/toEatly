c033d2f40898b8d2f7a2bb196dee16b929b6ee95
goodbedford <bedford@goodbedford.com>
2015-10-14 22:06:39 -0700

    add serialize to ajax

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,35 @@
 // CLIENT-SIDE JAVASCRIPT
 // On page load
 $(document).ready(function(){
-  
+
+  $('#new-food-form').on('submit', function(event){
+    event.preventDefault();
+    var newFood = $(this).serialize();
+    // var newFood = {};
+    // newFood.name = $('#name').val();
+    // newFood.yumminess = $('#yumminess').val();
+    console.log(newFood);
+    $.ajax({
+      url: '/api/foods/',
+      method: 'POST',
+      data: newFood,
+      success: function(data){
+        addFood(data);
+      },
+      error: function(xhr, ajaxOptions, thrownError){
+        console.log(thrownError);
+      }
+    })
+    .done(function(data){
+      $('form')[0].reset();
+    });
+  });
+  $(document).on('click', 'button.close', function(e){
+
+  });
+
+  function addFood( data){
+              var htmlString = '<li><strong>' +data.name+'</strong>:'+data.yumminess+'</li>';
+          $('#food-ul').append(htmlString);
+  }
 });
